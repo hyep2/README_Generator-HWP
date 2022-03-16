53e3175c5464942d0997530675a7dadbe70b9312
Hye Won Park <hyewonpark26@gmail.com>
2022-03-15 19:39:45 -0700

    imports necessary packages

---
+++ index.js
@@ -0,0 +1,35 @@
+//importing node packages
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown')
+
+// array of questions for user
+const questions = [ 
+  { 
+    type: 'input'
+    name: 
+    message:
+  },
+  {
+
+  },
+  {
+
+  },
+  {
+
+  }
+
+];
+
+// function to write README file
+function writeToFile(fileName, data) {
+}
+
+// function to initialize program
+function init() {
+
+}
+
+// function call to initialize program
+init();
