6ae8e29bc00efdcf74208dbe04be37a70222aa4e
Hye Won Park <hyewonpark26@gmail.com>
2022-03-15 20:22:45 -0700

    adds all the questions for user

--- index.js
+++ index.js
@@ -2,24 +2,63 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const generateMarkdown = require('./utils/generateMarkdown')
+const path = require('path');
 
 // array of questions for user
 const questions = [ 
   { 
-    type: 'input'
-    name: 
-    message:
+    type: 'input',
+    name: 'email',
+    message: 'What is your email address?'
   },
   {
-
+    type: 'input',
+    name: 'github',
+    message: 'What is your github username?'
   },
   {
-
+    type: 'input',
+    name: 'title',
+    message: 'What is the name of your project?',
   },
   {
-
+    type: 'input',
+    name: 'description',
+    message: "Write a short description of your project (ie. project's function, motivation for project, what problems does it solve)?",
+  },
+  {
+    type: 'input',
+    name: 'technologies',
+    message: 'What languages and libraries did you use?',
+  },
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'What command should be run to install dependencies?',
+    default: 'npm i',
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'Provide short instructions or examples so user can use the project.',
+  },
+  {
+    type: 'list',
+    name: 'license'
+    message: 'What kind of license should your project have?',
+    choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None'],
+  },
+  {
+    type: 'input',
+    name: 'contributing',
+    message: 'What does the user need to know about contributing to the repo?',
+  },
+  {
+    type: 'input',
+    name: 'test',
+    message: 'What command should be run to run tests?',
+    default: 'npm test',
   }
-
 ];
 
 // function to write README file
